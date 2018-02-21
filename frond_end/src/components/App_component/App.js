59d694a3df7c86c48b337c60a4ec4a11ff720ccb
venillas <sri.vanessa93@gmail.com>
2018-02-21 20:50:35 +0100

    Ajout de <<Router>> vers la page d'accueil + page d'inscriptio, possibilité de faire autrement, à voir plus tard si necessaire, restant à faire partie page d'accueil : refaire le formulaire de connexion

--- frond_end/src/components/App_component/App.js
+++ frond_end/src/components/App_component/App.js
@@ -1,61 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./App.css";
 import logo from '../../logo.svg';
-import './App.css';
 
-import { BrowserRouter } from 'react-router-dom'
-import { Route, Link } from 'react-router-dom'
-import Inscription from "../Inscription_component/Inscription";
-
-const BaseLayout = () => (
-  <div className="base">
-    <header>
-      <p>React Router v4 Browser Example</p>
-      <nav>
-        <ul>
-          <li><Link to='/'>Home</Link></li>
-          <li><Link to='/about'>About</Link></li>
-          <li><Link to='/me'>Profile</Link></li>
-          <li><Link to='/login'>Login</Link></li>
-          <li><Link to='/register'>Register</Link></li>
-          <li><Link to='/contact'>Contact</Link></li>
-
-        </ul>
-      </nav>
-    </header>
-    <div className="container">
-      <Route path="/" exact component={HomePage} />
-      <Route path="/about" component={AboutPage} />
-      <Route path="/contact" component={ContactPage} />
-      <Route path="/login" component={LoginPage} />
-      <Route path="/register" component="{RegisterPage}" />
-      <Route path="/me" component={ProfilePage} />
-    </div>
-    <footer>
-      React Router v4 Browser Example (c) 2017
-    </footer>
-  </div>
-)
-const HomePage = () => <div>This is a Home Page</div>
-const LoginPage = () => <div>This is a Login Page</div>
-const RegisterPage = () => <div>This is a Register Page</div>
-const ProfilePage = () => <div>This is the Profile Page</div>
-const AboutPage = () => <div>This is an About Page</div>
-const ContactPage = () => <div>This is a Contact Page</div>
 class App extends Component {
   constructor(props) {
     super(props);
-
     /*value correspondant au champs Identifiant*/
     this.state = { value: '' };
-
     /*A modifier: la var pour le mot de passe dans le this.state*/
     this.state = { pass: '' };
-
     /* actions */
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
-
   /*action onChange pour le champs Identifiant */
   handleChange(event) {
     this.setState({ value: event.target.value });
@@ -67,41 +25,35 @@ class App extends Component {
     event.preventDefault();
   }
 
-  /*Le Rendu*/
   render() {
     return (
-      // <div className="App">
-      //   <header className="App-header">
-      //     <img src={logo} className="App-logo" alt="logo" />
-      //     <h1 className="App-title">Bienvenue sur la plateforme pédagogique des STL de l'Université Pierre et Marie Curie </h1>
-      //   </header>
-      //   <p className="App-intro">
-      //     Notre plateforme vous permettra d'accéder avec facilité à l'ensemble des supports de cours des UEs de la filière STL.
-      //    </p>
-      //   <div>
-      //     <form onSubmit={this.handleSubmit}>
-      //       <label>
-      //         Identifiant:
-      //      <input type="text" value={this.state.value} onChange={this.handleChange} />
-      //       </label>
-      //       <br />
-      //       <br />
-      //       <label>
-      //         Mot de passe:
-      //      <input type="password" value={this.state.pass} />
-      //       </label>
-      //       <br />
-      //       <br />
 
-      //       <input type="submit" value="Submit" />
-      //     </form>
-      //   </div>
-      // </div>
+      <div className="App">
+        <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Bienvenue sur la plateforme pédagogique des STL de l'Université Pierre et Marie Curie </h1>
+        </header>
+        <p className="App-intro">
+          Notre plateforme vous permettra d'accéder avec facilité à l'ensemble des supports de cours des UEs de la filière STL.
+        </p>
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Identifiant:
+     <input type="text" value={this.state.value} onChange={this.handleChange} />
+            </label>
+            <br />
+            <label>
+              Mot de passe:
+     <input type="password" value={this.state.pass} />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
 
+          Vous n'avez pas de compte, Inscrivez vous ! <Link to="/01" style={{ color: 'blue' }} activeStyle={{ color: 'red' }}>Inscription</Link>
 
-      <BrowserRouter>
-        <BaseLayout />
-      </BrowserRouter>
+        </div>
+      </div>
 
     );
   }
--- frond_end/src/index.js
+++ frond_end/src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import { BrowserRouter } from 'react-router-dom'
-import { Route } from 'react-router-dom'
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
+import App from "./components/App_component/App";
+import registerServiceWorker from "./registerServiceWorker";
+import "./index.css";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Inscription from "./components/Inscription_component/Inscription";
 
 
-import App from './components/App_component/App';
-import Inscription from './components/Inscription_component/Inscription';
-
-ReactDOM.render(<App/>,
-    document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+    <div>
+        <Route path="/" exact component={App} />
+        <Route path="/01" component={Inscription} />
+    </div>
+  </Router>,
+  document.getElementById("root")
+);
 registerServiceWorker();
