6dfd2f1427a22396a38e55d4eb7b7f920aedbdff
Marie Laporte <mmarie.laporte@gmail.com>
2018-02-27 15:16:41 +0100

    Adding events recurrence

--- frond_end/src/components/Calendar_component/Calendar.js
+++ frond_end/src/components/Calendar_component/Calendar.js
@@ -17,20 +17,92 @@ class Calendar extends Component {
       var jcalData = ICAL.parse(data.trim());
       var comp = new ICAL.Component(jcalData);
       var eventComps = comp.getAllSubcomponents("vevent");
+      var eventsRec = [];
+      var exdates = [];
+      var recs = [];
+      var allRecs;
+      var exdate, r;
+      
+      for (var i = 0, l = eventComps.length; i < l; i++) {
+        exdate = eventComps[i].getFirstPropertyValue("exdate");
+        r = eventComps[i].getFirstPropertyValue("recurrence-id");
+        if (exdate !== null)
+          exdates.push(exdate);
+        if (r !== null)
+          recs.push(r);
+      }
+      allRecs = recs.concat(exdates);
       
       var events = $.map(eventComps, function (item) {
+        var expand = new ICAL.RecurExpansion({
+          component: item,
+          dtstart: item.getFirstPropertyValue('dtstart')
+        });
+
+        var next;
+        var i = 0, max = 100; // Limit for infinite recursion
+        var start = item.getFirstPropertyValue("dtstart");
+        var end = item.getFirstPropertyValue("dtend");
+        var location = item.getFirstPropertyValue("location");
+        var title = item.getFirstPropertyValue("summary") + (location ? "\n" + item.getFirstPropertyValue("location") : "");
+        
+        while (i < max && (next = expand.next())) {
+          var timeStart = new ICAL.Time({
+            year: next._time.year,
+            month: next._time.month,
+            day: next._time.day,
+            hour: start._time.hour,
+            minute: start._time.minute,
+          });
+          var timeEnd = new ICAL.Time({
+            year: next._time.year,
+            month: next._time.month,
+            day: next._time.day,
+            hour: end._time.hour,
+            minute: end._time.minute,
+          });
+
+          if (start.compare(timeStart) !== 0 && end.compare(timeEnd) !== 0) {
+            var test = false;
+            for (var j = 0, l = allRecs.length; j < l; j++) {
+              if (allRecs[j].compare(next) === 0) {
+                test = true;
+                break;
+              }
+            }
+            if (!test) {
+              eventsRec.push({
+                "title": title,
+                "start": timeStart.toJSDate(),
+                "end": timeEnd.toJSDate()
+              });
+            }
+          }
+        }
+        
         if (item.getFirstPropertyValue("class") == "PRIVATE") {
           return null;
         }
         else {
-          var location = item.getFirstPropertyValue("location");
-          return {
-            "title": item.getFirstPropertyValue("summary") + (location ? "\n" + item.getFirstPropertyValue("location") : ""),
-            "start": item.getFirstPropertyValue("dtstart").toJSDate(),
-            "end": item.getFirstPropertyValue("dtend").toJSDate()
-          };
+          var test = false;
+          for (var j = 0, l = exdates.length; j < l; j++) {
+            if (exdates[j].compare(start) === 0 && exdates[j].compare(end) === 0) {
+              test = true;
+              break;
+            }
+          }
+          if (!test) {
+            return {
+              "title": title,
+              "start": start.toJSDate(),
+              "end": end.toJSDate()
+            };
+          }
+          else
+            return null;
         }
       });
+      events = events.concat(eventsRec);
       $(calendar).fullCalendar(
         {
           events: events,
